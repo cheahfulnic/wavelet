17535bcc780f2e98d7ebe7384e512e31b49507af
nicholas cheah <nicholasseancheah@gmail.com>
2023-01-18 12:20:21 -0800

    Update NumberServer.java

--- NumberServer.java
+++ NumberServer.java
@@ -7,14 +7,15 @@ class Handler implements URLHandler {
     int num = 0;
 
     public String handleRequest(URI url) {
-        if (url.getPath().equals("/")) {
-            return String.format("Nicholas' number: %d", num); //returns 'Number: 0' when there is no path
-        } else if (url.getPath().equals("/increment")) {
+        if (url.getPath().equals("/")) { //returns 'Number: 0' when there is no path
+            return String.format("Nicholas' number: %d", num);
+        } else if (url.getPath().equals("/increment")) { //increments num and returns 'Number incremented!' when path is /increment
             num += 1;
-            return String.format("Number incremented!"); //increments num and returns 'Number incremented' when path is /increment
-        } else {
+            return String.format("Number incremented!");
+        } else { //returns '404 Not Found' if path is not /, /increment or a valid /add path
             System.out.println("Path: " + url.getPath());
-            if (url.getPath().contains("/add")) {
+            if (url.getPath().contains("/add")) { //increments num by the number after the equals sign and returns 'Number increased by', 
+                                                  //followed by the number, '! It's now', and then the sum of the original num and your number.
                 String[] parameters = url.getQuery().split("=");
                 if (parameters[0].equals("count")) {
                     num += Integer.parseInt(parameters[1]);
